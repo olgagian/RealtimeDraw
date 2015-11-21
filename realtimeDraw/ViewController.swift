ebd4a0e84bf579450b792279a510663914506866
ioannis giannakidis <olgagian@otenet.gr>
2015-11-21 15:58:36 +0200

    Initial Commit

---
+++ realtimeDraw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  realtimeDraw
+//
+//  Created by ioannis giannakidis on 21/11/15.
+//  Copyright © 2015 ioannis giannakidis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
